253fde5cbc2032caa4bb8ec1940e4160898d5b3e
TochusC <205329624@qq.com>
2024-03-24 16:08:22 +0800

    Allow all host to connect

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server: {
+    host: '0.0.0.0',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
