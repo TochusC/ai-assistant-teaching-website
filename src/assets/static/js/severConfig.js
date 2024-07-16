6068e211748d157474903ddee8bd97e46563363d
TochusC <205329624@qq.com>
2024-07-17 06:20:48 +0800

    Change server url.

--- src/assets/static/js/severConfig.js
+++ src/assets/static/js/severConfig.js
@@ -1,9 +1,9 @@
-const llmUrl = 'http://localhost:8000/';
+const llmUrl = 'http://1.13.159.238:8000/';
 const llmApi = llmUrl + 'chat/llm/';
 const basicLLMApi = llmUrl + 'chat/llm/basic/';
 const recognizeApi = llmUrl + 'chat/recognize/';
 const synthesizeApi = llmUrl + 'chat/synthesis/';
 
-const backendUrl = 'http://localhost:8080/';
+const backendUrl = 'http://1.13.159.238:8080/';
 
 export { llmUrl, llmApi, recognizeApi, synthesizeApi, backendUrl, basicLLMApi};
